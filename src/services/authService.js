f2aa28ba77c46cab074ff36f1d88ba6aec12ddc9
pzcastillo <pzcastillo@filmetricscorp.com>
2026-01-06 14:43:42 +0800

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,28 +1,10 @@
-const db = require('../db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+const { findAccountByUsernameOrEmail } = require('../repo/accountRepo');
 
 async function login({ usernameOrEmail, password }) {
-  const q = `
-    SELECT 
-      a.id,
-      a.username,
-      a.email,
-      a.fullname,
-      a.password_hash,
-      a.status,
-      a.role_id,
-      a.user_type_id,
-      r.role_name,
-      ut.type_name AS user_type_name
-    FROM accounts a
-    LEFT JOIN roles r ON a.role_id = r.id
-    LEFT JOIN user_types ut ON a.user_type_id = ut.id
-    WHERE a.username = $1 OR a.email = $1
-  `;
-
-  const r = await db.query(q, [usernameOrEmail]);
+  const r = await findAccountByUsernameOrEmail(usernameOrEmail);
 
   if (r.rowCount === 0) throw { status: 401, message: 'Invalid credentials' };
 
@@ -37,10 +19,8 @@ async function login({ usernameOrEmail, password }) {
   const tokenPayload = {
     sub: user.id,
     username: user.username,
-
     role_id: user.role_id,
     role_name: user.role_name,
-
     user_type_id: user.user_type_id,
     user_type_name: user.user_type_name
   };
